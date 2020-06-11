9d7d92072f960e8e12c075a78f124f69596a4e3d
Franz <fkrekeler@gmail.com>
2020-06-11 13:16:37 +0200

    added customLogger

---
+++ index.js
@@ -0,0 +1,58 @@
+const Log = require('@dazn/lambda-powertools-logger')
+
+
+
+module.exports = (thresholdMillis = 10,customLogger=undefined) => {
+  const createTimer = (event, context, thresholdMillis) => {
+  if (typeof context.getRemainingTimeInMillis !== 'function') {
+    return null
+  }
+
+  const timeLeft = context.getRemainingTimeInMillis()
+  const timeoutMs = timeLeft - thresholdMillis
+  const timer = setTimeout(() => {
+    const awsRequestId = context.awsRequestId
+    const invocationEvent = JSON.stringify(event)
+    if(typeof customLogger !== 'undefined'){
+      customLogger('invocation timed out', { awsRequestId, invocationEvent })
+    }
+    else{
+    Log.error('invocation timed out', { awsRequestId, invocationEvent })      
+    }
+  }, timeoutMs)
+
+  return timer
+}
+
+const hasTimer = (context) => {
+  return context.lambdaPowertoolsLogTimeoutMiddleware &&
+    context.lambdaPowertoolsLogTimeoutMiddleware.timer
+}
+
+
+  return {
+    before: (handler, next) => {
+      const timer = createTimer(handler.event, handler.context, thresholdMillis)
+      Object.defineProperty(handler.context, 'lambdaPowertoolsLogTimeoutMiddleware', {
+        enumerable: false,
+        value: { timer }
+      })
+
+      next()
+    },
+    after: (handler, next) => {
+      if (hasTimer(handler.context)) {
+        clearTimeout(handler.context.lambdaPowertoolsLogTimeoutMiddleware.timer)
+      }
+
+      next()
+    },
+    onError: (handler, next) => {
+      if (hasTimer(handler.context)) {
+        clearTimeout(handler.context.lambdaPowertoolsLogTimeoutMiddleware.timer)
+      }
+
+      next(handler.error)
+    }
+  }
+}
